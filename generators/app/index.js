9a52149f6439be083e2205c843d8e3e72c6f9d3a
Emil Okkels Klein <eok@pentia.dk>
2017-01-13 13:03:31 +0100

    add template and file creation for pentia project

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
 var yeoman = require('yeoman-generator');
 var mkdir = require('mkdirp');
 var yosay = require('yosay');
+var guid = require('uuid');
 
 module.exports = class extends yeoman {
 
@@ -15,9 +16,11 @@ module.exports = class extends yeoman {
         this.templatedata = {};
     }
 
-    askForSolutionType() {
-        var done = this.async();
+    _promptQuestions(questions) {
 
+    }
+
+    askForSolutionType() {
         var questions = [{
         type: 'list',
         name: 'type',
@@ -32,35 +35,11 @@ module.exports = class extends yeoman {
             }]
         }];
 
-        this.prompt(questions).then(function(answers) {
-            this.props = answers;
-        }.bind(this));
-    }
-
-    _promptQuestions(questions) {
         var done = this.async();
         this.prompt(questions).then(function(answers) {
-            this.props = answers;
+            this.type = answers.type;
             done();
         }.bind(this));
-    }
-
-    askForSolutionType() {
-        var questions = [{
-        type: 'list',
-        name: 'type',
-        message: 'What type of solution do you want to create?',
-        choices: [
-            {
-            name: 'Empty Helix solution',
-            value: 'emptyhelix'
-            }, {
-            name: 'Helix solution with Pentia tools',
-            value: 'pentiahelix'
-            }]
-        }];
-
-        this._promptQuestions(questions);
     }
 
     askForSolutionSettings() {
@@ -81,17 +60,54 @@ module.exports = class extends yeoman {
                 message: 'Would you like me to setup your package.json for you?'
             }];
 
-        this._promptQuestions(questions);
+        var done = this.async();
+        this.prompt(questions).then(function(answers) {
+            this.settings = answers;
+            this._buildTemplateData();
+            done();
+        }.bind(this));
     }
 
-    writing() {
-            var layers = [ 'Project', 'Feature', 'Foundation'];
+    _buildTemplateData() {
+        this.templatedata.solutionname = this.settings.SolutionName;
+        this.templatedata.environmentguid = guid.v4();
+    }
+
+
+    _writeLayers() {
+        var layers = [ 'Project', 'Feature', 'Foundation'];
 
             for(var i = 0; i < layers.length; i++) {
                 var layer = layers[i];
                 mkdir.sync(layer);
                 var layerDocumentationPath = layer + '/' + layer + '-layer.md'
                 this.fs.copy(this.templatePath(layerDocumentationPath),this.destinationPath(layerDocumentationPath));
+            }
+    }
+
+    _copySolutionItems() {
+        mkdir.sync("Project/Environment/Properties");
+        this.fs.copy(this.templatePath('_gulpfile.js'), this.destinationPath("gulpfile.js"));
+        this.fs.copy(this.templatePath('Project/Environment/web.config'), this.destinationPath("Project/Environment/web.config"));
+        this.fs.copy(this.templatePath('Project/Environment/packages.config'), this.destinationPath("Project/Environment/packages.config"));
+        this.fs.copy(this.templatePath('Project/Environment/Properties/AssemblyInfo.cs'), this.destinationPath("Project/Environment/Properties/AssemblyInfo.cs"));
+        this.fs.copyTpl(this.templatePath('_solution.sln'), this.destinationPath(this.settings.SolutionName + ".sln"), this.templatedata);
+        this.fs.copyTpl(this.templatePath('_package.json'), this.destinationPath("package.json"), this.templatedata);
+        this.fs.copyTpl(this.templatePath('Project/Environment/Environment.csproj'), this.destinationPath("Project/Environment/Environment.csproj"), this.templatedata);
+    }
+
+    writing() {
+            this._writeLayers();
+            console.log(this.type);
+            switch(this.type)
+            {
+                case 'emptyhelix':
+
+                break;
+
+                case 'pentiahelix':
+                    this._copySolutionItems() 
+                break;
             }
         }
 };
