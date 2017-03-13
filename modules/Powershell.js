0327a24d099966113109da814e8dd872b0228184
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-13 15:54:15 +0100

    Rewrite powershell.js as a class
    We use classes for the rest of the generator, so let's use it here as
    well.

--- modules/Powershell.js
+++ modules/Powershell.js
@@ -1,31 +1,32 @@
-/*jslint node: true  */
-"use strict";
-var yeoman = require('yeoman-generator');
+/*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+'use strict';
 
-function Powershell () {
-}
+class Powershell {
+	constructor() {
+	}
 
-Powershell.prototype.runAsync = function (pathToScriptFile, parameters) {
-  console.log("Powershell - running: " + pathToScriptFile + " " + parameters);
-  var spawn = require("child_process").spawn;
-  var child = spawn("powershell.exe", [pathToScriptFile.replace(/ /g,'` '), parameters]);
+	runAsync(pathToScriptFile, parameters) {
+		console.log('Powershell - running: ' + pathToScriptFile + ' ' + parameters);
+		var spawn = require('child_process').spawn;
+		var child = spawn('powershell.exe', [pathToScriptFile.replace(/ /g,'` '), parameters]);
 
-  child.stdout.setEncoding('utf8');
-  child.stderr.setEncoding('utf8');
-  
-  child.stdout.on("data", function (data) {
-    console.log(data);
-  });
-  
-  child.stderr.on("data", function (data) {
-    console.log("Error: " + data);
-  });
+		child.stdout.setEncoding('utf8');
+		child.stderr.setEncoding('utf8');
 
-  child.on("exit", function () {
-    console.log("Powershell - done running " + pathToScriptFile + parameters);
-  });
+		child.stdout.on('data', function (data) {
+			console.log(data);
+		});
 
-  child.stdin.end();
+		child.stderr.on('data', function (data) {
+			console.error('Error: ' + data);
+		});
+
+		child.on('exit', function () {
+			console.log('Powershell - done running ' + pathToScriptFile + parameters);
+		});
+
+		child.stdin.end();
+	}
 }
 
 exports = module.exports = new Powershell();
